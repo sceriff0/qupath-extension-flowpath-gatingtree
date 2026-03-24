0b79730a89627810e588281efb3e0624a95f81ec
bolt3x <valeriofassi@hotmail.it>
2026-03-24 16:53:51 +0100

    🐛 forgottt

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 qupathExtension {
     name = "FlowPath"
     group = "io.github.qupath"
-    version = "0.4.1"
+    version = "0.5.0"
     description = "Interactive tree-based cell phenotyping gating for multiplexed imaging"
     automaticModule = "qupath.ext.flowpath"
 }
